5d7c15187ba7567285248fe728643f015d0d6257
Trinity Quirk <tquirk@ymb.net>
2014-07-12 15:01:07 -0500

    Starting on the OSX client
    We've created an Xcode project in the client directory, which will house
    the OSX client program.  Not much there yet, just wanted to get the files
    committed into this new branch.

---
+++ client/r9/r9/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  r9
+//
+//  Created by Trinity Quirk on 7/11/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
