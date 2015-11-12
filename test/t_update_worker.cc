2afb171d61932d5b41ccf367848f9f8eae5ab37e
Trinity Quirk <tquirk@ymb.net>
2015-11-12 08:32:29 -0600

    Initial update-worker test
    The test doesn't currently do anything, but this commit contains
    cleanups in the Makefile.am, along with the usual .gitignore
    changes, and a skeleton test file (which currently fails).
    
    This starts out issue #15.

---
+++ test/t_update_worker.cc
@@ -0,0 +1,11 @@
+#include "../server/classes/zone.h"
+#include "../server/classes/listensock.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+std::vector<listen_socket *> sockets;
+
+TEST(UpdateWorkerTest, Operate)
+{
+}
