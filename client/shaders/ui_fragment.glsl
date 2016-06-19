fa980a6cf489f47792aef64ddbe287aa4505e285
Trinity Quirk <tquirk@ymb.net>
2016-06-19 17:23:32 -0500

    Update shaders to use texture UV coordinates
    The label uses textures, so our shaders should know about them.
    The last two elements of the input array are the coordinates, so
    we just pass them straight through to the fragment shader.

--- client/shaders/ui_fragment.glsl
+++ client/shaders/ui_fragment.glsl
@@ -1,12 +1,21 @@
 #version 330 core
 
 in vec4 vcolor;
+in vec2 tex_coord;
 
 out vec4 fcolor;
 
-uniform uint use_text;
+uniform sampler2D tex;
+uniform int use_text;
 
 void main()
 {
-    fcolor = vcolor;
+    if (tex_coord != vec2(-1.0, -1.0))
+    {
+        fcolor = texture(tex, tex_coord);
+        if (use_text == 1)
+            fcolor = fcolor.rrrr * vcolor;
+    }
+    else
+        fcolor = vcolor;
 }
--- client/shaders/ui_vertex.glsl
+++ client/shaders/ui_vertex.glsl
@@ -5,9 +5,11 @@ in vec4 color;
 in vec2 texture_uv;
 
 out vec4 vcolor;
+out vec2 tex_coord;
 
 void main()
 {
     vcolor = color;
+    tex_coord = texture_uv;
     gl_Position = vec4(position, 0.0, 1.0);
 }
