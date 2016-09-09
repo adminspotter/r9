35239ab427c9804955ff8630bf9915b8eb71eec4
Trinity Quirk <tquirk@ymb.net>
2016-09-09 07:43:06 -0500

    Implement client login dialog callback
    When we get an ok, we want to do the actual login.  We don't
    actually call this callback anywhere yet, but it needs to become a
    real commit.  It has existed as a stashed patch for months now;
    about time that it became a real commit.
    
    Re: issue #124

--- client/comm_dialog.cc
+++ client/comm_dialog.cc
@@ -1,6 +1,6 @@
 /* comm_dialog.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 08 Sep 2016, 23:18:14 tquirk
+ *   last updated 09 Sep 2016, 07:38:34 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -27,6 +27,13 @@
  *
  */
 
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+
+#include "client.h"
+
 #include "ui/text_field.h"
 
 static ui::font *dialog_font;
@@ -38,6 +45,27 @@ void create_login_dialog(ui::context *ctx)
 
 void setup_comm_callback(ui::event_target *t, void *call, void *client)
 {
+    struct addrinfo hints, *ai;
+    std::string host_str, user_str, pass_str;
+
+    if (host->get(ui::element::string, 0, &host_str) != 0
+        || user->get(ui::element::string, 0, &user_str) != 0
+        || pass->get(ui::element::string, 0, &pass_str) != 0)
+        return;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    snprintf(portstr, sizeof(portstr), "%d", config.server_port);
+    if ((ret = getaddrinfo(host_str.c_str(), portstr, &hints, &ai)) != 0)
+    {
+        std::cout << "Couldn't find host " << host_str
+                  << ": " << gai_strerror(ret) << " (" << ret << ')'
+                  << std::endl;
+        return -1;
+    }
+    setup_comm(ai, user_str.c_str(), pass_str.c_str(), config.charname.c_str());
 }
 
 void close_dialog_callback(ui::event_target *t, void *call, void *client)
