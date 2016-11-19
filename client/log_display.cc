566462684eff230be8b95788e06c3adb0c12fee0
Trinity Quirk <tquirk@ymb.net>
2016-11-19 10:49:43 -0600

    Implement the log window creation stub
    We now have a basic log window that won't do anything.  We'll add
    some more functions in upcoming commits which will give the window
    some actual functionality.
    
    Re: issue #64

--- client/log_display.cc
+++ client/log_display.cc
@@ -1,6 +1,6 @@
 /* log_display.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 16 Nov 2016, 18:33:09 tquirk
+ *   last updated 19 Nov 2016, 10:44:34 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -25,16 +25,37 @@
  * labels for each entry.  We'll age each entry out after a
  * configurable time.
  *
+ * We want the widget to remain at the bottom left corner of the
+ * window, and grow and shrink upwards as needed.  We have the resize
+ * callback list now, that we can use for this purpose.
+ *
  * Things to do
  *
  */
+
+#include "configdata.h"
 
 #include "ui/ui.h"
 #include "ui/row_column.h"
 #include "ui/multi_label.h"
 
+#define DISTANCE_FROM_BOTTOM 10
+
+static ui::font *log_font;
 static ui::row_column *log_window;
 
 void create_log_window(ui::context *ctx)
 {
+    glm::ivec2 grid(0, 1), spacing(15, 10), pos(10, 0);
+    int packing = ui::order::column;
+
+    log_font = new ui::font(config.font_name, 13, config.font_paths);
+
+    ctx->get(ui::element::size, ui::size::height, &pos.y);
+    pos.y -= DISTANCE_FROM_BOTTOM;
+    log_window = new ui::row_column(ctx, 10, 0);
+    log_window->set_va(ui::element::order, 0, &packing,
+                       ui::element::child_spacing, ui::size::all, &spacing,
+                       ui::element::size, ui::size::grid, &grid,
+                       ui::element::position, ui::position::all, &pos, 0);
 }
