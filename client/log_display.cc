a62d74e02308ade92115e32d1f6884537a25c877
Trinity Quirk <tquirk@ymb.net>
2017-01-16 23:10:22 -0600

    Full refactor of the log display widget
    This is the full refactor of the log display into its own widget.
    It was not possible to break this up in a meaningful way, so this
    commit is way too big.

--- client/log_display.cc
+++ client/log_display.cc
@@ -1,6 +1,6 @@
 /* log_display.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 25 Nov 2016, 18:13:15 tquirk
+ *   last updated 08 Jan 2017, 08:27:30 tquirk
  *
  * Revision IX game client
  * Copyright (C) 2016  Trinity Annabelle Quirk
@@ -23,169 +23,220 @@
  * This file contains the UI widgets to display log entries.  We'll
  * have a mostly-stationary row-column, with individual multiline
  * labels for each entry.  We'll age each entry out after a
- * configurable time.
+ * configurable time, with individual widget timeouts.
  *
  * We want the widget to remain at the bottom left corner of the
  * window, and grow and shrink upwards as needed.  We have the resize
  * callback list now, that we can use for this purpose.
  *
  * Things to do
+ *   - Make the lifetime and font size config items.
  *
  */
 
-#include <unistd.h>
-#include <pthread.h>
+#include <math.h>
 
 #include <sstream>
 #include <stdexcept>
+#include <utility>
 
 #include "configdata.h"
-#include "logbuf.h"
-#include "l10n.h"
 
-#include "ui/ui.h"
-#include "ui/row_column.h"
+#include "ui/ui_defs.h"
+#include "log_display.h"
 #include "ui/multi_label.h"
 
 #define DISTANCE_FROM_BOTTOM 10
-#define ENTRY_LIFETIME 10 * 1000000000
+#define LABEL_WIDTH 225
+#define ENTRY_LIFETIME 10
 
-typedef struct entry_tag
+static void resize_pos_callback(ui::active *, void *, void *);
+
+std::ostream& operator<<(std::ostream& o, logbuf::lb_entry& l)
 {
-    logbuf::lb_entry *log_entry;
-    ui::widget *label;
-
-    const struct entry_tag& operator=(const struct entry_tag& et)
-        {
-            this->log_entry = et.log_entry;
-            this->label = et.label;
-            return *this;
-        }
+    o << "timestamp: " << l.timestamp.time_since_epoch().count() << std::endl;
+    o << "wall time: " << l.display_time.time_since_epoch().count() << std::endl;
+    o << "string:    " << l.entry << std::endl;
+    return o;
 }
-entry;
 
-static void *clean_old_log_entries(void *);
-static void context_resize_log_pos_callback(ui::active *, void *, void *);
-
-static pthread_t log_cleanup_thread;
-static pthread_mutex_t log_lock = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t log_not_empty = PTHREAD_COND_INITIALIZER;
-static bool exit_flag = false;
-static std::list<entry> entries;
-static ui::font *log_font;
-static ui::row_column *log_window;
-static const std::chrono::nanoseconds lifetime(ENTRY_LIFETIME);
-
-void create_log_window(ui::context *ctx)
+void *log_display::cleanup_entries(void *arg)
 {
-    glm::ivec2 grid(0, 1), spacing(15, 10), pos(10, 0);
-    int packing = ui::order::column, ret;
+    log_display *ld = (log_display *)arg;
+    logbuf::lb_ts_point now;
+    std::chrono::duration<float> ftime;
+    float ftime_val;
+    struct timespec ts;
 
-    log_font = new ui::font(config.font_name, 13, config.font_paths);
+    for (;;)
+    {
+        if (ld->entries.size() == 0)
+            sleep(ENTRY_LIFETIME);
+        else
+        {
+            /* Do a nanosleep until the head entry expires. */
+            ftime = ld->entries.front().log_entry->timestamp
+                + ld->entry_lifetime - logbuf::lb_ts_time::now();
+            ftime_val = ftime.count();
+            if (ftime_val > 0.0f)
+            {
+                ts.tv_sec = (int)truncf(ftime_val);
+                ftime_val -= (float)ts.tv_sec;
+                ts.tv_nsec = (int)truncf(ftime_val * 1000000000);
+                nanosleep(&ts, NULL);
+            }
+        }
 
-    ctx->get(ui::element::size, ui::size::height, &pos.y);
-    pos.y -= DISTANCE_FROM_BOTTOM;
-    log_window = new ui::row_column(ctx, 10, 0);
-    log_window->set_va(ui::element::order, 0, &packing,
-                       ui::element::child_spacing, ui::size::all, &spacing,
-                       ui::element::size, ui::size::grid, &grid,
-                       ui::element::position, ui::position::all, &pos, 0);
-    log_window->add_callback(ui::callback::resize,
-                             context_resize_log_pos_callback, NULL);
+        /* See if anything needs closing */
+        if (ld->entries.size() == 0)
+            continue;
 
-    if ((ret = pthread_create(&log_cleanup_thread, NULL,
-                              clean_old_log_entries, NULL)) != 0)
+        now = logbuf::lb_ts_time::now();
+        while (now >= (ld->entries.front().log_entry->timestamp
+                       + ld->entry_lifetime))
+        {
+            pthread_mutex_lock(&ld->queue_mutex);
+
+            ld->entries.front().label->close();
+            ld->entries.pop_front();
+
+            if (ld->entries.size() == 0)
+                ld->created = ld->entries.end();
+
+            pthread_mutex_unlock(&ld->queue_mutex);
+        }
+    }
+}
+
+log_display::log_display(ui::composite *p, GLuint w, GLuint h)
+    : ui::row_column(p, w, h), ui::rect(w, h), entries(),
+      entry_lifetime(ENTRY_LIFETIME)
+{
+    int border = 1, ret;
+
+    this->created = this->entries.end();
+    this->queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+    if ((ret = pthread_create(&(this->cleanup_thread), NULL,
+                              log_display::cleanup_entries, (void *)this)) != 0)
     {
         std::ostringstream s;
-        s << _("Could not create log cleanup thread.") << std::endl;
+        s << "Couldn't start log display cleanup thread: "
+          << strerror(ret) << " (" << ret << ")";
         throw std::runtime_error(s.str());
     }
-    std::clog.rdbuf(new logbuf());
+
+    this->grid_sz = glm::ivec2(1, 0);
+    this->child_spacing = glm::ivec2(5, 10);
+    this->pos = glm::ivec2(10, 0);
+
+    this->pack_order = ui::order::column;
+
+    this->log_font = new ui::font(config.font_name, 13, config.font_paths);
+
+    this->composite::parent->get(ui::element::size,
+                                 ui::size::height,
+                                 &this->pos.y);
+    this->pos.y -= DISTANCE_FROM_BOTTOM;
+
+    this->background = glm::vec4(0.0, 0.0, 0.0, 0.0);
+
+    this->add_callback(ui::callback::resize, resize_pos_callback, NULL);
+    this->orig_rdbuf = std::clog.rdbuf(new logbuf());
 }
 
-void cleanup_log_window(void)
+log_display::~log_display()
 {
     int ret;
 
-    exit_flag = true;
-    pthread_cond_broadcast(&log_not_empty);
-    if ((ret = pthread_join(log_cleanup_thread, NULL)) != 0)
-    {
-        std::ostringstream s;
-        s << _("Could not join log cleanup thread.") << std::endl;
-        throw std::runtime_error(s.str());
-    }
-    log_window->close();
+    /* Set std::clog's rdbuf back to a normal state, in case we need
+     * to emit some errors to it.
+     */
+    delete std::clog.rdbuf(this->orig_rdbuf);
+
+    if ((ret = pthread_cancel(this->cleanup_thread)) != 0)
+        std::clog << "Couldn't cancel log display cleanup thread: "
+                  << strerror(ret) << " (" << ret << ")" << std::endl;
+    sleep(0);
+    if ((ret = pthread_join(this->cleanup_thread, NULL)) != 0)
+        std::clog << "Couldn't reap log display cleanup thread: "
+                  << strerror(ret) << " (" << ret << ")" << std::endl;
+    if ((ret = pthread_mutex_destroy(&this->queue_mutex)) != 0)
+        std::clog << "Couldn't destroy log display mutex: "
+                  << strerror(ret) << " (" << ret << ")" << std::endl;
 }
 
-void add_log_entry(logbuf::lb_entry *lbe)
+void log_display::add_entry(logbuf::lb_entry *lbe)
 {
-    entry ent;
-    int border = 1, orig_pos, orig_height, new_height;
-    ui::multi_label *ml = new ui::multi_label(log_window, 150, 0);
+    log_display::entry ent;
 
+    std::cout << "addr is " << std::hex << (void *)lbe << std::dec << std::endl;
+    std::cout << "entry is " << *lbe << std::endl;;
     ent.log_entry = lbe;
-    ent.label = ml;
+    ent.label = NULL;
+    pthread_mutex_lock(&this->queue_mutex);
+    this->entries.push_back(ent);
+    pthread_mutex_unlock(&this->queue_mutex);
+}
 
-    log_window->get_va(ui::element::position, ui::position::y, &orig_pos,
-                       ui::element::size, ui::size::height, &orig_height, 0);
-    ml->set_va(ui::element::font, ui::ownership::shared, log_font,
-               ui::element::border, ui::side::all, &border,
-               ui::element::string, 0, &lbe->entry, 0);
-    log_window->get(ui::element::size, ui::size::height, &new_height);
-    orig_pos -= new_height - orig_height;
-    log_window->set(ui::element::position, ui::position::y, &orig_pos);
+void log_display::create_log_labels(void)
+{
+    if (this->entries.size() == 0
+        || (this->created != this->entries.end()
+            && this->created == --this->entries.end()))
+        return;
 
-    pthread_mutex_lock(&log_lock);
-    entries.push_back(std::move(ent));
-    if (entries.size() != 0)
-        pthread_cond_signal(&log_not_empty);
-    pthread_mutex_unlock(&log_lock);
+    while (this->created != --this->entries.end())
+    {
+        if (this->created == this->entries.end())
+        {
+            std::cout << "setting begin" << std::endl;
+            this->created = this->entries.begin();
+            std::cout << "front is " << *this->entries.front().log_entry << std::endl;
+        }
+        else
+        {
+            std::cout << "incrmenting" << std::endl;
+            ++this->created;
+        }
+
+        int border = 2, orig_pos = this->pos.y, orig_height = this->dim.y;
+        this->created->label = new ui::multi_label(this, LABEL_WIDTH, 0);
+
+        std::cout << "new addr is " << std::hex << (void *)(this->created->log_entry) << std::dec << std::endl;
+        std::cout << "new ent is " << *this->created->log_entry << std::endl;
+        this->created->label->set_va(
+            ui::element::font, ui::ownership::shared, this->log_font,
+            ui::element::border, ui::side::all, &border,
+            ui::element::string, 0, &this->created->log_entry->entry, 0);
+        orig_pos -= this->dim.y - orig_height;
+        this->set_position(ui::position::y, &orig_pos);
+    }
+}
+
+void log_display::draw(GLuint trans_uniform, const glm::mat4& parent_trans)
+{
+    /* We must lock the mutex here so that the deletion thread won't
+     * try to delete something while we're trying to draw it.  This is
+     * the reason we did the subclassing in the first place.
+     */
+    pthread_mutex_lock(&this->queue_mutex);
+    this->create_log_labels();
+    this->row_column::draw(trans_uniform, parent_trans);
+    pthread_mutex_unlock(&this->queue_mutex);
 }
 
 /* ARGSUSED */
-void *clean_old_log_entries(void *arg)
-{
-    struct timespec ts;
-    std::chrono::nanoseconds d;
-
-    sleep(10);
-    for (;;)
-    {
-        pthread_mutex_lock(&log_lock);
-        while (entries.empty() && !exit_flag)
-            pthread_cond_wait(&log_not_empty, &log_lock);
-
-        if (exit_flag)
-        {
-            pthread_mutex_unlock(&log_lock);
-            pthread_exit(NULL);
-        }
-
-        d = lifetime + entries.front().log_entry->timestamp
-            - logbuf::lb_ts_time::now();
-        ts.tv_sec = std::chrono::duration_cast<std::chrono::seconds>(d).count();
-        ts.tv_nsec = d.count() - (ts.tv_sec * 1000000000);
-        std::cout << "sleeping " << ts.tv_sec << " seconds, " << ts.tv_nsec << " nsec" << std::endl;
-        nanosleep(&ts, NULL);
-        entries.front().label->close();
-        entries.pop_front();
-        pthread_mutex_unlock(&log_lock);
-    }
-    return NULL;
-}
-
-void context_resize_log_pos_callback(ui::active *a, void *call, void *client)
+void resize_pos_callback(ui::active *a, void *call, void *client)
 {
     ui::resize_call_data *call_data = (ui::resize_call_data *)call;
-    ui::row_column *lw = dynamic_cast<ui::row_column *>(a);
+    log_display *ld = dynamic_cast<log_display *>(a);
 
-    if (lw == log_window)
+    if (ld != NULL)
     {
         int log_height;
 
-        lw->get(ui::element::size, ui::size::height, &log_height);
+        ld->get(ui::element::size, ui::size::height, &log_height);
         log_height = call_data->new_size.y - log_height - DISTANCE_FROM_BOTTOM;
-        lw->set(ui::element::position, ui::position::y, &log_height);
+        ld->set(ui::element::position, ui::position::y, &log_height);
     }
 }
