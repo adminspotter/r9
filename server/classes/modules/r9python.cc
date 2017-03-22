ae7c59653a629235494337bb14df595a77dad0de
Trinity Quirk <tquirk@ymb.net>
2017-03-22 10:17:04 -0500

    Add Python implementation file
    We've added the header, so this is the implementation file.  All
    of the methods are stubbed out.
    
    Re:  issue #27

---
+++ server/classes/modules/r9python.cc
@@ -0,0 +1,41 @@
+/* r9python.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 22 Mar 2017, 10:14:11 tquirk
+ *
+ * Revision IX game server
+ * Copyright (C) 2017  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file contains the class implementation for embedding python.
+ *
+ * Things to do
+ *
+ */
+
+#include "r9python.h"
+
+PythonLanguage::PythonLanguage()
+{
+}
+
+PythonLanguage::~PythonLanguage()
+{
+}
+
+std::string PythonLanguage::execute(const std::string &s)
+{
+}
