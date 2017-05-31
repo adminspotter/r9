0576fb11dde1dde048fd3cb42e5bfb495fb9438b
Trinity Quirk <tquirk@ymb.net>
2017-05-31 17:47:23 -0500

    Add test for bad socket return in server basesock
    There are a bunch of exceptional conditions in the server basesock
    which we're not testing.  This is the first nibble, testing what
    happens when the socket() call fails.
    
    We'll have to mock a bunch of system functions here, and start with
    socket(), setsockopt(), and bind() here.

--- test/t_basesock.cc
+++ test/t_basesock.cc
@@ -7,6 +7,8 @@
 
 #include <gtest/gtest.h>
 
+bool socket_error = false, socket_zero = false, bind_error = false;
+
 void *test_thread_worker(void *arg)
 {
     basesock *base = (basesock *)arg;
@@ -22,6 +24,30 @@ void *test_thread_worker(void *arg)
     return NULL;
 }
 
+int socket(int a, int b, int c)
+{
+    if (socket_error == true)
+        return -1;
+    if (socket_zero == true)
+        return 0;
+    return 123;
+}
+
+int setsockopt(int a, int b, int c, const void *d, socklen_t e)
+{
+    return 0;
+}
+
+int bind(int a, const struct sockaddr *b, socklen_t c)
+{
+    if (bind_error == true)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    return 0;
+}
+
 TEST(BasesockTest, CreateDelete)
 {
     struct addrinfo hints, *ai;
@@ -35,6 +61,7 @@ TEST(BasesockTest, CreateDelete)
     ret = getaddrinfo("localhost", "1235", &hints, &ai);
     ASSERT_EQ(ret, 0);
 
+    socket_zero = true;
     ASSERT_NO_THROW(
         {
             base = new basesock(ai);
@@ -66,6 +93,30 @@ TEST(BasesockTest, CreateDelete)
         delete(base);
         freeaddrinfo(ai);
     }
+    socket_zero = false;
+}
+
+TEST(BasesockTest, BadSocket)
+{
+    struct addrinfo hints, *ai;
+    int ret;
+    basesock *base;
+
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
+    ret = getaddrinfo("localhost", "1235", &hints, &ai);
+    ASSERT_EQ(ret, 0);
+
+    socket_error = true;
+    ASSERT_THROW(
+        {
+            base = new basesock(ai);
+        },
+        std::runtime_error);
+    socket_error = false;
+    freeaddrinfo(ai);
 }
 
 TEST(BasesockTest, StartStop)
