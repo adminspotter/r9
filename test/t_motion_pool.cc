bb7433a4800255da0aaf6c9b7db43cda6a7456c2
Trinity Quirk <tquirk@ymb.net>
2017-06-01 21:36:28 -0500

    Improve coverage of the server motion pool unit test
    There are a few lines within the conditional which decides if an
    object is still moving that aren't being directly hit, so we'll
    contrive some new objects which move in ways that will use these
    conditions.

--- test/t_motion_pool.cc
+++ test/t_motion_pool.cc
@@ -12,13 +12,25 @@ TEST(MotionPoolTest, Operate)
 {
     Zone *zone = new Zone(1000LL, 1);
     MotionPool *pool = new MotionPool("t_motion", 1);
-    GameObject *go = new GameObject(NULL, NULL, 9876LL);
+    GameObject *go1 = new GameObject(NULL, NULL, 9876LL);
+    GameObject *go2 = new GameObject(NULL, NULL, 9877LL);
+    GameObject *go3 = new GameObject(NULL, NULL, 9878LL);
 
-    go->position = { 234.0, 234.0, 234.0 };
-    go->movement = { 1.0, 1.0, 1.0 };
-    gettimeofday(&go->last_updated, NULL);
-    pool->push(go);
+    go1->position = { 234.0, 234.0, 234.0 };
+    go1->movement = { 1.0, 1.0, 1.0 };
+    gettimeofday(&go1->last_updated, NULL);
+    pool->push(go1);
     ASSERT_EQ(pool->queue_size(), 1);
+    go2->position = { 123.0, 123.0, 123.0 };
+    go2->movement = { 0.0, 1.0, 1.0 };
+    gettimeofday(&go2->last_updated, NULL);
+    pool->push(go2);
+    ASSERT_EQ(pool->queue_size(), 2);
+    go3->position = { 12.0, 12.0, 12.0 };
+    go3->movement = { 0.0, 0.0, 1.0 };
+    gettimeofday(&go3->last_updated, NULL);
+    pool->push(go3);
+    ASSERT_EQ(pool->queue_size(), 3);
 
     delete zone->motion_pool;
     zone->motion_pool = pool;
@@ -27,9 +39,12 @@ TEST(MotionPoolTest, Operate)
     sleep(1);
     zone->motion_pool->stop();
 
-    ASSERT_GT(go->position.x, 234.0);
-    ASSERT_GT(go->position.y, 234.0);
-    ASSERT_GT(go->position.z, 234.0);
+    ASSERT_GT(go1->position.x, 234.0);
+    ASSERT_GT(go1->position.y, 234.0);
+    ASSERT_GT(go1->position.z, 234.0);
+    ASSERT_GT(go2->position.y, 123.0);
+    ASSERT_GT(go2->position.z, 123.0);
+    ASSERT_GT(go3->position.z, 12.0);
 
     delete zone;
 }
