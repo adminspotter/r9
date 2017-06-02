f51a7b6bca7380313de90f1f33fdeda81ad98a98
Trinity Quirk <tquirk@ymb.net>
2017-06-02 09:18:48 -0500

    Add create/delete test for the server GameObject
    This is the start of a test for the server's GameObject class.
    Just a simple create/delete test.  We've got mocking in place so
    that we can test pthread mutex failures, but we notice that we have
    no such checking in the object.
    
    Re:  issue #147

---
+++ test/t_game_obj.cc
@@ -0,0 +1,45 @@
+#include "../server/classes/game_obj.h"
+
+#include <gtest/gtest.h>
+
+bool pthread_mutex_lock_error = false, pthread_mutex_unlock_error = false;
+int lock_count, unlock_count;
+
+int pthread_mutex_lock(pthread_mutex_t *a)
+{
+    ++lock_count;
+    if (pthread_mutex_lock_error == true)
+        return EINVAL;
+    return 0;
+}
+
+int pthread_mutex_unlock(pthread_mutex_t *a)
+{
+    ++unlock_count;
+    if (pthread_mutex_unlock_error == true)
+        return EINVAL;
+    return 0;
+}
+
+TEST(GameObjTest, CreateDelete)
+{
+    GameObject *go = NULL;
+    Geometry *geom = new Geometry();
+    Control *con = new Control(1LL, NULL);
+
+    lock_count = unlock_count = 0;
+    go = new GameObject(geom, con, 38LL);
+    ASSERT_EQ(go->get_object_id(), 38LL);
+    ASSERT_TRUE(go->master == con);
+    ASSERT_TRUE(go->geometry == geom);
+    ASSERT_EQ(lock_count, unlock_count);
+
+    delete go;
+
+    geom = new Geometry();
+    go = new GameObject(geom, con);
+    ASSERT_EQ(go->get_object_id(), 39LL);
+
+    delete go;
+    delete con;
+}
