df22ecb9d11aacebdc59fa085065bda095ffedc5
Trinity Quirk <tquirk@ymb.net>
2017-06-26 18:25:38 -0500

    Add gmocked listen_socket for testing
    Since some of the objects contain listen_socket objects, we should
    have a mocked-out version we can use for testing those objects.

---
+++ test/mock_listensock.h
@@ -0,0 +1,30 @@
+#ifndef __INC_MOCK_LISTENSOCK_H__
+#define __INC_MOCK_LISTENSOCK_H__
+
+#include <gmock/gmock.h>
+
+#include "../server/classes/listensock.h"
+
+class mock_listen_socket : public listen_socket
+{
+  public:
+    mock_listen_socket(struct addrinfo *a) : listen_socket(a) {};
+    virtual ~mock_listen_socket() {};
+
+    MOCK_METHOD0(init, void(void));
+    MOCK_METHOD0(start, void(void));
+    MOCK_METHOD0(stop, void(void));
+
+    MOCK_METHOD1(login_user, void(access_list&));
+    MOCK_METHOD1(logout_user, void(access_list&));
+
+    MOCK_METHOD4(do_login, void(uint64_t, Control *, access_list&, int));
+
+    MOCK_METHOD3(send_ack, void(Control *, uint8_t, uint8_t));
+
+    /* Shouldn't need to mock the access_pool_worker static method,
+     * since the fake start/stop methods won't do anything.
+     */
+};
+
+#endif /* __INC_MOCK_LISTENSOCK_H__ */
