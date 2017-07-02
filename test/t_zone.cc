b8b770f99d24fccbe764320d3839eb02529a58b0
Trinity Quirk <tquirk@ymb.net>
2017-07-02 14:10:16 -0500

    Set some fake positions for fake objects in the server zone test
    We're going to do a little testing with objects in the zone, so
    we'll give the objects position data, so we get coverage of those
    areas of the connect method.

--- test/t_zone.cc
+++ test/t_zone.cc
@@ -10,8 +10,15 @@ using ::testing::Invoke;
 
 int fake_server_objects(std::map<uint64_t, GameObject *>& gom)
 {
+    glm::dvec3 pos(100.0, 100.0, 100.0);
+
     gom[1234LL] = new GameObject(NULL, NULL, 1234LL);
+    gom[1234LL]->position = pos;
+
     gom[1235LL] = new GameObject(NULL, NULL, 1235LL);
+    pos.x = 125.0;
+    gom[1235LL]->position = pos;
+
     return 2;
 }
 
