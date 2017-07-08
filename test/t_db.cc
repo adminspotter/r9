9a6dcfec7d24a000320b7230c82e56e5958cce82
Trinity Quirk <tquirk@ymb.net>
2017-07-08 11:56:11 -0500

    Add mocked system functions to server database test
    The failure modes we want to test are bad returns from a handful
    of system functions, so we need to make fake functions that do
    what we want.

--- test/t_db.cc
+++ test/t_db.cc
@@ -2,6 +2,50 @@
 
 #include <gtest/gtest.h>
 
+bool gethostname_failure = false, getaddrinfo_failure = false;
+bool ntop_failure = false;
+const char address[] = "1.2.3.4";
+
+int gethostname(char *a, size_t b)
+{
+    if (gethostname_failure == true)
+    {
+        errno = EPERM;
+        return -1;
+    }
+    strncpy(a, "whut.foo.com", b);
+    return 0;
+}
+
+/* This function will leak memory like crazy, but it's just a test and
+ * will terminate quickly and never do anything else, so it's just not
+ * important.
+ */
+int getaddrinfo(const char *a, const char *b,
+                const struct addrinfo *c, struct addrinfo **d)
+{
+    if (getaddrinfo_failure == true)
+        return EAI_BADFLAGS;
+    *d = new struct addrinfo;
+    (*d)->ai_family = AF_INET;
+    (*d)->ai_addr = (struct sockaddr *)new struct sockaddr_in;
+    ((struct sockaddr_in *)(*d)->ai_addr)->sin_addr.s_addr = 1;
+    return 0;
+}
+
+void freeaddrinfo(struct addrinfo *a)
+{
+    delete a->ai_addr;
+    delete a;
+}
+
+const char *inet_ntop(int a, const void *b, char *c, socklen_t d)
+{
+    if (ntop_failure == true)
+        return NULL;
+    return address;
+}
+
 class fake_DB : public DB
 {
     fake_DB(const std::string& a, const std::string& b,
