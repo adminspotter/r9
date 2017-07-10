a4ea6abc982a97c03f9f4b2f75a253e0232d2620
Trinity Quirk <tquirk@ymb.net>
2017-07-09 19:32:17 -0500

    Add create/delete test for server config_data
    We've had a test for the client config object for quite some time,
    but no test for the server-side config object.  First test makes
    sure that create and delete work, and that the defaults are set as
    expected.

---
+++ test/t_config_data.cc
@@ -0,0 +1,47 @@
+#include "../server/config_data.h"
+
+#include <gtest/gtest.h>
+
+/* There is a global config_data object that comes in with
+ * config_data.cc, but we won't use it.  We'll make our own new ones
+ * for each test, so we don't have to worry about previous state.
+ */
+
+TEST(ConfigDataTest, CreateDelete)
+{
+    config_data *conf;
+
+    ASSERT_NO_THROW(
+        {
+            conf = new config_data;
+        });
+
+    ASSERT_EQ(conf->server_root, config_data::SERVER_ROOT);
+    ASSERT_EQ(conf->log_prefix, config_data::LOG_PREFIX);
+    ASSERT_EQ(conf->pid_fname, config_data::PID_FNAME);
+    ASSERT_EQ(conf->db_type, config_data::DB_TYPE);
+    ASSERT_EQ(conf->db_host, config_data::DB_HOST);
+    ASSERT_EQ(conf->db_name, config_data::DB_NAME);
+    ASSERT_EQ(conf->action_lib, config_data::ACTION_LIB);
+    ASSERT_EQ(conf->daemonize, true);
+    ASSERT_EQ(conf->use_keepalive, false);
+    ASSERT_EQ(conf->use_nonblock, false);
+    ASSERT_EQ(conf->use_reuse, true);
+    ASSERT_EQ(conf->use_linger, config_data::LINGER_LEN);
+    ASSERT_EQ(conf->log_facility, config_data::LOG_FACILITY);
+    ASSERT_EQ(conf->load_threshold, config_data::LOAD_THRESH);
+    ASSERT_EQ(conf->min_subservers, config_data::MIN_SUBSERV);
+    ASSERT_EQ(conf->max_subservers, config_data::MAX_SUBSERV);
+    ASSERT_EQ(conf->access_threads, config_data::NUM_THREADS);
+    ASSERT_EQ(conf->motion_threads, config_data::NUM_THREADS);
+    ASSERT_EQ(conf->send_threads, config_data::NUM_THREADS);
+    ASSERT_EQ(conf->update_threads, config_data::NUM_THREADS);
+    ASSERT_EQ(conf->size.dim[0], config_data::ZONE_SIZE);
+    ASSERT_EQ(conf->size.dim[1], config_data::ZONE_SIZE);
+    ASSERT_EQ(conf->size.dim[2], config_data::ZONE_SIZE);
+    ASSERT_EQ(conf->size.steps[0], config_data::ZONE_STEPS);
+    ASSERT_EQ(conf->size.steps[1], config_data::ZONE_STEPS);
+    ASSERT_EQ(conf->size.steps[2], config_data::ZONE_STEPS);
+
+    delete conf;
+}
