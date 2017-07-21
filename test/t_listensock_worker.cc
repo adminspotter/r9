871a2bcd0edb6c0ad8e8e517a7122554950962d8
Trinity Quirk <tquirk@ymb.net>
2017-07-21 08:54:44 -0500

    Add some mocking in preparation for testing listen_socket reaper
    The reaper sleeps a lot, so we need to mock out sleep.  It's not
    nearly as crazy as it sounds, and will allow our test to run very
    quickly.
    
    Also, we'll need to construct more than one listen_socket, so we'll
    copy in our function to make an addrinfo object that won't collide
    with other sockets in the same file.
    
    Finally, we'll need the config object, and we'll also need the
    database pointer and stuff, so we'll include our mocked server
    globals header.

--- test/t_listensock_worker.cc
+++ test/t_listensock_worker.cc
@@ -1,8 +1,44 @@
 #include "../server/classes/listensock.h"
+#include "../server/config_data.h"
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <gtest/gtest.h>
 
-int login_count, logout_count;
+#include "mock_server_globals.h"
+
+int sleep_count, login_count, logout_count;
+
+unsigned int sleep(unsigned int a)
+{
+    ++sleep_count;
+    return a;
+}
+
+struct addrinfo *create_addrinfo(void)
+{
+    struct addrinfo hints, *addr = NULL;
+    static int port = 9876;
+    char port_str[6];
+    int ret;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_protocol = 0;
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    snprintf(port_str, sizeof(port_str), "%d", port--);
+    if ((ret = getaddrinfo(NULL, port_str, &hints, &addr)) != 0)
+    {
+        std::cerr << gai_strerror(ret) << std::endl;
+        throw std::runtime_error("getaddrinfo broke");
+    }
+
+    return addr;
+}
 
 class test_listen_socket : public listen_socket
 {
