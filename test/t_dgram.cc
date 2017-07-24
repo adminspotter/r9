f09696db8a4a72de70f855b08ba004431697f58e
Trinity Quirk <tquirk@ymb.net>
2017-07-23 23:24:42 -0500

    Add test for server dgram user assignment
    We have an assignment operator in the server datagram user object,
    so let's test it.
    
    Re:  issue #163

--- test/t_dgram.cc
+++ test/t_dgram.cc
@@ -17,3 +17,26 @@ TEST(DgramUserTest, CreateDelete)
     delete dgu;
     delete control;
 }
+
+TEST(DgramUserTest, Assignment)
+{
+    Control *control1 = new Control(0LL, NULL);
+    Control *control2 = new Control(123LL, NULL);
+    dgram_user *dgu1, *dgu2;
+
+    dgu1 = new dgram_user(control1->userid, control1);
+    dgu2 = new dgram_user(control2->userid, control2);
+
+    ASSERT_NE(dgu1->userid, dgu2->userid);
+    ASSERT_NE(dgu1->control, dgu2->control);
+
+    *dgu2 = *dgu1;
+
+    ASSERT_EQ(dgu1->userid, dgu2->userid);
+    ASSERT_EQ(dgu1->control, dgu2->control);
+
+    delete dgu2;
+    delete dgu1;
+    delete control2;
+    delete control1;
+}
