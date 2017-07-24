b42041d70a317bca157ea40871cc4939013ef3f8
Trinity Quirk <tquirk@ymb.net>
2017-07-24 08:11:29 -0500

    Add create/delete test for server datagram socket
    We're into testing the datagram socket itself.  First test is
    whether we can create and delete it correctly.
    
    Re:  issue #163

--- test/t_dgram.cc
+++ test/t_dgram.cc
@@ -4,6 +4,31 @@
 
 #include "mock_server_globals.h"
 
+struct addrinfo *create_addrinfo(void)
+{
+    struct addrinfo hints, *addr = NULL;
+    static int port = 8765;
+    char port_str[6];
+    int ret;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_NUMERICSERV;
+    hints.ai_protocol = 0;
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    snprintf(port_str, sizeof(port_str), "%d", port--);
+    if ((ret = getaddrinfo(NULL, port_str, &hints, &addr)) != 0)
+    {
+        std::cerr << gai_strerror(ret) << std::endl;
+        throw std::runtime_error("getaddrinfo broke");
+    }
+
+    return addr;
+}
+
 TEST(DgramUserTest, CreateDelete)
 {
     Control *control = new Control(0LL, NULL);
@@ -39,4 +64,18 @@ TEST(DgramUserTest, Assignment)
     delete dgu1;
     delete control2;
     delete control1;
+}
+
+TEST(DgramSocketTest, CreateDelete)
+{
+    struct addrinfo *addr = create_addrinfo();
+    dgram_socket *dgs;
+
+    ASSERT_NO_THROW(
+        {
+            dgs = new dgram_socket(addr);
+        });
+
+    delete dgs;
+    freeaddrinfo(addr);
 }
