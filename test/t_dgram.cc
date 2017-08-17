6466ba5ad7037a4d3c43844ab8abc2b999c6a165
Trinity Quirk <tquirk@ymb.net>
2017-08-16 22:36:37 -0500

    Remove dgram_user tests
    There is no more dgram_user, so there is no need for tests.
    
    Re:  issue #173

--- test/t_dgram.cc
+++ test/t_dgram.cc
@@ -33,43 +33,6 @@ struct addrinfo *create_addrinfo(void)
     return addr;
 }
 
-TEST(DgramUserTest, CreateDelete)
-{
-    Control *control = new Control(0LL, NULL);
-    dgram_user *dgu;
-
-    ASSERT_NO_THROW(
-        {
-            dgu = new dgram_user(control->userid, control, NULL);
-        });
-
-    delete dgu;
-    delete control;
-}
-
-TEST(DgramUserTest, Assignment)
-{
-    Control *control1 = new Control(0LL, NULL);
-    Control *control2 = new Control(123LL, NULL);
-    dgram_user *dgu1, *dgu2;
-
-    dgu1 = new dgram_user(control1->userid, control1, NULL);
-    dgu2 = new dgram_user(control2->userid, control2, NULL);
-
-    ASSERT_NE(dgu1->userid, dgu2->userid);
-    ASSERT_NE(dgu1->control, dgu2->control);
-
-    *dgu2 = *dgu1;
-
-    ASSERT_EQ(dgu1->userid, dgu2->userid);
-    ASSERT_EQ(dgu1->control, dgu2->control);
-
-    delete dgu2;
-    delete dgu1;
-    delete control2;
-    delete control1;
-}
-
 TEST(DgramSocketTest, CreateDelete)
 {
     struct addrinfo *addr = create_addrinfo();
