644d937ce161da997013eab8e287ef16bab2302b
Trinity Quirk <tquirk@ymb.net>
2017-09-08 07:41:37 -0500

    Add key and mouse maps in client
    We've got some high cyclomatic complexity in a couple of the client
    GLFW-to-UI mapping functions, and those are best resolved by using
    map data structures to do the mapping.  We'll add the structures
    now, and handle the refactoring in a future commit.

--- client/client.cc
+++ client/client.cc
@@ -1,9 +1,9 @@
 /* client.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 24 Jan 2017, 08:34:29 tquirk
+ *   last updated 08 Sep 2017, 07:36:17 tquirk
  *
  * Revision IX game client
- * Copyright (C) 2016  Trinity Annabelle Quirk
+ * Copyright (C) 2017  Trinity Annabelle Quirk
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -33,6 +33,7 @@
 
 #include <iostream>
 #include <vector>
+#include <map>
 
 #include <glm/vec2.hpp>
 
@@ -51,6 +52,37 @@ void char_callback(GLFWwindow *, unsigned int, int);
 void mouse_position_callback(GLFWwindow *, double, double);
 void mouse_button_callback(GLFWwindow *, int, int, int);
 void resize_callback(GLFWwindow *, int, int);
+void move_key_callback(ui::active *, void *, void *);
+void stop_key_callback(ui::active *, void *, void *);
+
+static std::map<int, int> glfw_key_map =
+{
+    { GLFW_KEY_LEFT, ui::key::l_arrow },
+    { GLFW_KEY_RIGHT, ui::key::r_arrow },
+    { GLFW_KEY_UP, ui::key::u_arrow },
+    { GLFW_KEY_DOWN, ui::key::d_arrow },
+    { GLFW_KEY_HOME, ui::key::home },
+    { GLFW_KEY_END, ui::key::end },
+    { GLFW_KEY_BACKSPACE, ui::key::bkspc },
+    { GLFW_KEY_DELETE, ui::key::del },
+    { GLFW_KEY_ESCAPE, ui::key::esc },
+    { GLFW_PRESS, ui::key::down },
+    { GLFW_RELEASE, ui::key::up }
+};
+
+static std::map<int, int> glfw_mouse_map =
+{
+    { GLFW_MOUSE_BUTTON_1, ui::mouse::button0 },
+    { GLFW_MOUSE_BUTTON_2, ui::mouse::button1 },
+    { GLFW_MOUSE_BUTTON_3, ui::mouse::button2 },
+    { GLFW_MOUSE_BUTTON_4, ui::mouse::button3 },
+    { GLFW_MOUSE_BUTTON_5, ui::mouse::button4 },
+    { GLFW_MOUSE_BUTTON_6, ui::mouse::button5 },
+    { GLFW_MOUSE_BUTTON_7, ui::mouse::button6 },
+    { GLFW_MOUSE_BUTTON_8, ui::mouse::button7 },
+    { GLFW_PRESS, ui::mouse::down },
+    { GLFW_RELEASE, ui::mouse::up }
+};
 
 std::vector<Comm *> comm;
 ConfigData config;
