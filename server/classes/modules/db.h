377bbc77a4c85659baf2ea64640b54fa93d4e6c7
Trinity Quirk <tquirk@ymb.net>
2017-11-28 07:25:25 -0600

    Add second pure virtual method to get DB auth via charname
    When performing a login, we're trying to use the server game object
    ID in place of a character ID, which isn't working properly.  We'll
    use the character name, which we do have at the time we're trying
    to log the user in, in place of the character ID, which we can't
    currently retrieve by any existing method.
    
    We will add the database-specific methods in upcoming commits.

--- server/classes/modules/db.h
+++ server/classes/modules/db.h
@@ -1,6 +1,6 @@
 /* db.h                                                    -*- C++ -*-
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 06 Jul 2017, 09:48:40 tquirk
+ *   last updated 28 Nov 2017, 07:12:06 tquirk
  *
  * Revision IX game server
  * Copyright (C) 2017  Trinity Annabelle Quirk
@@ -66,6 +66,7 @@ class DB
     virtual uint64_t check_authentication(const std::string&,
                                            const std::string&) = 0;
     virtual int check_authorization(uint64_t, uint64_t) = 0;
+    virtual int check_authorization(uint64_t, const std::string&) = 0;
     virtual uint64_t get_character_objectid(uint64_t, const std::string&) = 0;
     virtual int open_new_login(uint64_t, uint64_t, Sockaddr *) = 0;
     virtual int check_open_login(uint64_t, uint64_t) = 0;
