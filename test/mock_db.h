3fb5c7790db6f57153ddd4435759a6d05c0ea695
Trinity Quirk <tquirk@ymb.net>
2018-03-14 08:34:08 -0500

    Add fake DB for mocking purposes
    Since we're removing the need for gmock, we'll need to make fake
    objects that do nothing in place of the gmocked classes.  This is
    a fake database object.  For now, it's only meant to mock out a
    single method, but since almost all of the methods are pure-virtual
    we have to implement something for them.
    
    As we go along, we'll mock the methods that we need at the time
    when we need them, to keep the diffs (and conversions) simpler.

--- test/mock_db.h
+++ test/mock_db.h
@@ -29,4 +29,69 @@ class mock_DB : public DB
     MOCK_METHOD1(get_server_objects, int(std::map<uint64_t, GameObject *>&));
 };
 
+int check_authentication_count = 0;
+uint64_t check_authentication_result = 12345LL;
+int check_authorization_result = 0;
+uint64_t get_characterid_result = 0LL, get_character_objectid_result = 0LL;
+int open_new_login_result = 0, check_open_login_result = 0;
+int close_open_login_result = 0, get_player_server_skills_result = 0;
+int get_server_skills_result = 0, get_server_objects_result = 0;
+
+class fake_DB : public DB
+{
+  public:
+    fake_DB(const std::string& a, const std::string& b,
+            const std::string& c, const std::string& d) : DB(a, b, c, d) {};
+    virtual ~fake_DB() {};
+
+    virtual uint64_t check_authentication(const std::string& a,
+                                          const std::string& b)
+        {
+            ++check_authentication_count;
+            return check_authentication_result;
+        };
+    virtual int check_authorization(uint64_t a, uint64_t b)
+        {
+            return check_authorization_result;
+        };
+    virtual int check_authorization(uint64_t a, const std::string& b)
+        {
+            return check_authorization_result;
+        };
+    virtual uint64_t get_characterid(uint64_t a, const std::string& b)
+        {
+            return get_characterid_result;
+        };
+    virtual uint64_t get_character_objectid(uint64_t a,
+                                            const std::string& b)
+        {
+            return get_character_objectid_result;
+        };
+    virtual int open_new_login(uint64_t a, uint64_t b, Sockaddr *c)
+        {
+            return open_new_login_result;
+        };
+    virtual int check_open_login(uint64_t a, uint64_t b)
+        {
+            return check_open_login_result;
+        };
+    virtual int close_open_login(uint64_t a, uint64_t b, Sockaddr *c)
+        {
+            return close_open_login_result;
+        };
+    virtual int get_player_server_skills(uint64_t a, uint64_t b,
+                                         std::map<uint16_t, action_level>& c)
+        {
+            return get_player_server_skills_result;
+        };
+    virtual int get_server_skills(std::map<uint16_t, action_rec>& a)
+        {
+            return get_server_skills_result;
+        };
+    virtual int get_server_objects(std::map<uint64_t, GameObject *>& a)
+        {
+            return get_server_objects_result;
+        };
+};
+
 #endif /* __INC_MOCK_DB_H__ */
