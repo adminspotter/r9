8304b787937f407aae668917247a92eb86989442
Trinity Quirk <tquirk@ymb.net>
2018-03-16 21:35:02 -0500

    Add fake library object
    In order to get rid of the gmocked library, we'll make a fake one
    that just overrides everything in a reasonable and transparent way.

--- test/mock_library.h
+++ test/mock_library.h
@@ -42,4 +42,22 @@ class mock_Library : public Library
     MOCK_METHOD0(close, void(void));
 };
 
+int symbol_count = 0;
+void *symbol_result = NULL;
+
+class fake_Library : public Library
+{
+  public:
+    fake_Library(const std::string& a) : Library(a) {};
+    virtual ~fake_Library() {};
+
+    virtual void open(void) {};
+    virtual void *symbol(const char *a)
+        {
+            ++symbol_count;
+            return symbol_result;
+        };
+    virtual void close(void) {};
+};
+
 #endif /* __INC_MOCK_LIBRARY_H__ */
