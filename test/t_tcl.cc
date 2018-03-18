5c4e270ebf0c8899a0ef86ffc660360bbe83e5e8
Trinity Quirk <tquirk@ymb.net>
2018-03-18 15:45:58 -0500

    Convert base server tcl tests to use libtap++
    These are simple tests, so the conversion was straightforward.

--- test/t_tcl.cc
+++ test/t_tcl.cc
@@ -1,8 +1,12 @@
-#include <gtest/gtest.h>
+#include <tap++.h>
+
+using namespace TAP;
 
 #include <config.h>
 #include "../server/classes/library.h"
 #include "../server/classes/modules/language.h"
+
+#include <string.h>
 
 #define TCL_MOD "../server/classes/modules/.libs/libr9_tcl" LT_MODULE_EXT
 
@@ -10,54 +14,74 @@ Language *(*create_language)(void);
 std::string (*execute_language)(Language *, const std::string&);
 void (*destroy_language)(Language *);
 
-TEST(TclTest, GoodConstructor)
+void test_good_constructor(void)
 {
+    std::string test = "good constructor: ";
     Language *lang = NULL;
 
-    ASSERT_NO_THROW(
-        {
-            lang = create_language();
-        });
-    ASSERT_TRUE(lang != NULL);
+    try
+    {
+        lang = create_language();
+    }
+    catch (...)
+    {
+        fail(test + "create exception");
+    }
+    is(lang != NULL, true, test + "language object created");
 
-    ASSERT_NO_THROW(
-        {
-            destroy_language(lang);
-        });
+    try
+    {
+        destroy_language(lang);
+    }
+    catch (...)
+    {
+        fail(test + "destroy exception");
+    }
 }
 
-TEST(TclTest, RunScript)
+void test_run_script(void)
 {
+    std::string test = "run script: ";
     Language *lang = NULL;
 
-    ASSERT_NO_THROW(
-        {
-            lang = create_language();
-        });
-    ASSERT_TRUE(lang != NULL);
+    try
+    {
+        lang = create_language();
+    }
+    catch (...)
+    {
+        fail(test + "create exception");
+    }
+    is(lang != NULL, true, test + "language object created");
 
     /* From https://www.tcl.tk/about/language.html, creates a
      * procedure which raises the first arg to the power of the second
      * arg, which we then call to get 2^8.
      */
     std::string str = "proc power {base p} {\n    set result 1\n    while {$p > 0} {\n        set result [expr $result * $base]\n        set p [expr $p - 1]\n    }\n    return $result\n}\n\npower 2 8";
-    ASSERT_NO_THROW(
-        {
-            str = execute_language(lang, str);
-        });
-    ASSERT_FALSE(strcmp(str.c_str(), "256"));
+    try
+    {
+        str = execute_language(lang, str);
+    }
+    catch (...)
+    {
+        fail(test + "execute exception");
+    }
+    is(strcmp(str.c_str(), "256"), 0, test + "expected result");
 
-    ASSERT_NO_THROW(
-        {
-            destroy_language(lang);
-        });
+    try
+    {
+        destroy_language(lang);
+    }
+    catch (...)
+    {
+        fail(test + "destroy exception");
+    }
 }
 
-GTEST_API_ int main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    int retval;
-
-    testing::InitGoogleTest(&argc, argv);
+    plan(3);
 
     /* Load up the perl lib and fetch the symbols */
     Library *lib = new Library(TCL_MOD);
@@ -65,8 +89,10 @@ GTEST_API_ int main(int argc, char **argv)
     execute_language = (std::string (*)(Language *, const std::string &))lib->symbol("lang_execute");
     destroy_language = (void (*)(Language *))lib->symbol("destroy_language");
 
-    retval = RUN_ALL_TESTS();
+    test_good_constructor();
+    test_run_script();
+
     delete lib;
 
-    return retval;
+    return exit_status();
 }
