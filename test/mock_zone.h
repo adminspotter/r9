6d5d773aaec3258cf889a85b740a0d9092c2e10a
Trinity Quirk <tquirk@ymb.net>
2018-03-18 16:23:27 -0500

    Remove last elements of gtest from build configs
    We discovered the use of build-aux from the cuddly-gl project, and
    it segregates the build scripts from the rest of the codebase, so
    we'll do it here.  That moves the install-sh file into that
    directory.
    
    We add the tap-driver.sh script, which isn't normally installed as
    part of the autogen process.  That also lives in build-aux.
    
    The test makefiles now reference the tap-driver.sh script for the
    test log driver.
    
    There were a couple of small references to gtest and gmock which
    we neglected to remove previously.

--- test/mock_zone.h
+++ test/mock_zone.h
@@ -1,21 +1,7 @@
 #ifndef __INC_MOCK_ZONE_H__
 #define __INC_MOCK_ZONE_H__
 
-#include <gmock/gmock.h>
-
 #include "../server/classes/zone.h"
-
-class mock_Zone : public Zone
-{
-  public:
-    mock_Zone(uint64_t a, uint16_t b, DB *c) : Zone(a, b, c) {};
-    virtual ~mock_Zone() {};
-
-    MOCK_METHOD1(send_nearby_objects, void(uint64_t));
-
-    MOCK_METHOD1(sector_contains, Octree *(glm::dvec3&));
-    MOCK_METHOD1(which_sector, glm::ivec3(glm::dvec3&));
-};
 
 int send_nearby_objects_count = 0;
 Octree *sector_contains_result = NULL;
