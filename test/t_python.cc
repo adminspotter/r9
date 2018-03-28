a73df1fddfccee7d690e468be8b206f645664af4
Trinity Quirk <tquirk@ymb.net>
2018-03-28 08:35:00 -0500

    Add create/delete test for server embedded python object
    We need to make sure we can make more than one at a time, and
    delete them all again.
    
    Re:  issue #27

--- test/t_python.cc
+++ test/t_python.cc
@@ -13,16 +13,58 @@ lang_create_t create_language;
 lang_execute_t execute_language;
 lang_destroy_t destroy_language;
 
+void test_create_delete(void)
+{
+    std::string test = "create/delete: ";
+    Language *lang = NULL, *lang2 = NULL;
+
+    try
+    {
+        lang = create_language();
+    }
+    catch (...)
+    {
+        fail(test + "create exception");
+    }
+    is(lang != NULL, true, test + "language object created");
+    try
+    {
+        lang2 = create_language();
+    }
+    catch (...)
+    {
+        fail(test + "create exception");
+    }
+    is(lang2 != NULL, true, test + "second language object created");
+
+    try
+    {
+        destroy_language(lang2);
+    }
+    catch (...)
+    {
+        fail(test + "destroy exception");
+    }
+    try
+    {
+        destroy_language(lang);
+    }
+    catch (...)
+    {
+        fail(test + "destroy exception");
+    }
+}
+
 int main(int argc, char **argv)
 {
-    plan(0);
+    plan(2);
 
     Library *lib = new Library(PYTHON_MOD);
     create_language = (lang_create_t)lib->symbol("create_language");
     execute_language = (lang_execute_t)lib->symbol("lang_execute");
     destroy_language = (lang_destroy_t)lib->symbol("destroy_language");
 
-    /* Tests here */
+    test_create_delete();
 
     delete lib;
 
