8c15a6304e53466afa8ecd346804306c5322d4f8
Trinity Quirk <tquirk@ymb.net>
2018-04-06 10:07:53 -0500

    Add header for Diffie-Hellman messages
    Openssl uses mostly strings of unsigned chars along with a length,
    so we'll give ourselves a structure to pass these things around.

---
+++ proto/dh.h
@@ -0,0 +1,38 @@
+/* dh.h
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 06 Apr 2018, 09:58:52 tquirk
+ *
+ * Revision IX game server
+ * Copyright (C) 2018  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * Structures and function declarations for Diffie-Hellman key exchange.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9_PROTO_DH_H__
+#define __INC_R9_PROTO_DH_H__
+
+struct dh_message
+{
+    unsigned char *message;
+    size_t message_len;
+};
+
+#endif /* __INC_R9_PROTO_DH_H__ */
