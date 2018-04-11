25689af8bfc8e6d6204b9fc4535979895cf5ca19
Trinity Quirk <tquirk@ymb.net>
2018-04-11 09:37:15 -0500

    Implement ECDH key generator
    This function will generate a key along our chosen curve and return
    a pointer to the EVP_PKEY structure.
    
    We also add a happy-path test; the various exceptional conditions
    will require a bit more work to get full coverage.
    
    Re:  issue #31

--- proto/ec.c
+++ proto/ec.c
@@ -31,5 +31,28 @@
 
 EVP_PKEY *generate_ecdh_key(void)
 {
-    return NULL;
+    EVP_PKEY_CTX *param_ctx, *key_ctx;
+    EVP_PKEY *key = NULL, *params = NULL;
+
+    if ((param_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL)) == NULL)
+        goto BAILOUT1;
+
+    if (EVP_PKEY_paramgen_init(param_ctx) != 1
+        || EVP_PKEY_CTX_set_ec_paramgen_curve_nid(param_ctx, R9_CURVE) != 1
+        || EVP_PKEY_paramgen(param_ctx, &params) != 1)
+        goto BAILOUT2;
+
+    if ((key_ctx = EVP_PKEY_CTX_new(params, NULL)) == NULL)
+        goto BAILOUT3;
+    if (EVP_PKEY_keygen_init(key_ctx) == 1)
+        EVP_PKEY_keygen(key_ctx, &key);
+
+    EVP_PKEY_CTX_free(key_ctx);
+  BAILOUT3:
+    EVP_PKEY_free(params);
+  BAILOUT2:
+    EVP_PKEY_CTX_free(param_ctx);
+
+  BAILOUT1:
+    return key;
 }
--- test/t_ec.cc
+++ test/t_ec.cc
@@ -4,9 +4,21 @@ using namespace TAP;
 
 #include "../proto/ec.h"
 
+void test_generate_ecdh_key(void)
+{
+    std::string test = "generate_ecdh_key: ";
+
+    EVP_PKEY *result = generate_ecdh_key();
+
+    is(result != NULL, true, test + "expected result");
+
+    OPENSSL_free(result);
+}
+
 int main(int argc, char **argv)
 {
-    plan(0);
+    plan(1);
 
+    test_generate_ecdh_key();
     return exit_status();
 }
