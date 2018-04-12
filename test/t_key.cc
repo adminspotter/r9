cade515346acb92da0e6ae456ab0db92041e49dc
Trinity Quirk <tquirk@ymb.net>
2018-04-12 09:06:39 -0500

    Add string-to-key converter
    We need to go the other way and convert a string back into a key
    structure.  Now that we can reliably convert a key into a string,
    we can take that output and convert it back.
    
    In future commits, we should ensure that the key structure we get
    back from the converter is the same as the structure we initially
    sent into the key-to-string converter.
    
    Re:  issue #31

--- test/t_key.cc
+++ test/t_key.cc
@@ -18,10 +18,29 @@ void test_pkey_to_string(void)
     OPENSSL_free(key);
 }
 
+void test_string_to_pkey(void)
+{
+    std::string test = "string_to_pkey: ";
+
+    EVP_PKEY *key = generate_ecdh_key();
+
+    unsigned char str[1024];
+    size_t str_size = pkey_to_string(key, (unsigned char **)&str, sizeof(str));
+    isnt(str_size, 0, test + "string conversion correct");
+
+    EVP_PKEY *converted = string_to_pkey(str, str_size);
+
+    is(converted != NULL, true, test + "key conversion correct");
+
+    OPENSSL_free(converted);
+    OPENSSL_free(key);
+}
+
 int main(int argc, char **argv)
 {
-    plan(1);
+    plan(3);
 
     test_pkey_to_string();
+    test_string_to_pkey();
     return exit_status();
 }
