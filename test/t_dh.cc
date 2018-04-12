a28eb802cd1f5bdafd41e68676266a6cee29f5a7
Trinity Quirk <tquirk@ymb.net>
2018-04-12 08:20:42 -0500

    Add test for shared-secret generator
    Now that we have an ECDH key generator, we can generate a couple of
    keys to feed into the shared-secret generator for testing purposes.
    
    We'll worry about testing the exceptional conditions later, as
    that is going to be complicated.

--- test/t_dh.cc
+++ test/t_dh.cc
@@ -5,6 +5,7 @@ using namespace TAP;
 #include <string.h>
 
 #include "../proto/dh.h"
+#include "../proto/ec.h"
 
 unsigned char fake_data[] = "abcdefghijklmnopqrstuvwxyz0123456789";
 unsigned char fake_digest[] =
@@ -13,6 +14,25 @@ unsigned char fake_digest[] =
     0xcf, 0xb7, 0x71, 0x3d, 0xd5, 0xd3, 0x3e, 0x81, 0x76, 0x80, 0x88,
     0xfd, 0xd5, 0x57, 0xac, 0x74, 0x6b, 0x9a, 0x2a, 0x2f, 0xc7
 };
+
+void test_dh_shared_secret(void)
+{
+    std::string test = "dh_shared_secret: ";
+    EVP_PKEY *priv = generate_ecdh_key();
+    EVP_PKEY *peer = generate_ecdh_key();
+
+    is(priv != NULL, true, test + "generated private key");
+    is(peer != NULL, true, test + "generated peer key");
+
+    struct dh_message *msg = dh_shared_secret(priv, peer);
+
+    is(msg != NULL, true, test + "generated shared secret");
+
+    OPENSSL_free(msg->message);
+    OPENSSL_free(msg);
+    OPENSSL_free(peer);
+    OPENSSL_free(priv);
+}
 
 void test_digest_message(void)
 {
@@ -29,8 +49,9 @@ void test_digest_message(void)
 
 int main(int argc, char **argv)
 {
-    plan(1);
+    plan(4);
 
+    test_dh_shared_secret();
     test_digest_message();
     return exit_status();
 }
