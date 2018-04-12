8ec1adbf78a96529507bcf2fd9b76b302edcf5f1
Trinity Quirk <tquirk@ymb.net>
2018-04-12 08:52:13 -0500

    Add test driver for key conversions
    This adds the scaffolding - test driver file and the build configs
    for it.

---
+++ test/t_key.cc
@@ -0,0 +1,13 @@
+#include <tap++.h>
+
+using namespace TAP;
+
+#include "../proto/key.h"
+#include "../proto/ec.h"
+
+int main(int argc, char **argv)
+{
+    plan(0);
+
+    return exit_status();
+}
