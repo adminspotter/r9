4d7f9736356bc72230e0a1488c21917f7dcaf7f2
Trinity Quirk <tquirk@ymb.net>
2018-04-20 06:37:07 -0700

    Factor stream socket init into its own method
    We want to add a default constructor, which will perform the same
    initialization as the regular constructor, so we'll make that init
    available as its own method.

--- server/classes/stream.cc
+++ server/classes/stream.cc
@@ -1,6 +1,6 @@
 /* stream.cc
  *   by Trinity Quirk <tquirk@ymb.net>
- *   last updated 19 Apr 2018, 07:34:46 tquirk
+ *   last updated 20 Apr 2018, 05:34:00 tquirk
  *
  * Revision IX game server
  * Copyright (C) 2018  Trinity Annabelle Quirk
@@ -64,12 +64,17 @@ static std::map<int, listen_socket::packet_handler> packet_handlers =
     { TYPE_LGTREQ, listen_socket::handle_logout }
 };
 
-stream_socket::stream_socket(struct addrinfo *ai)
-    : listen_socket(ai), fds()
+void stream_socket::init(void)
 {
     FD_ZERO(&this->master_readfs);
     FD_SET(this->sock.sock, &this->master_readfs);
     this->max_fd = this->sock.sock + 1;
+}
+
+stream_socket::stream_socket(struct addrinfo *ai)
+    : listen_socket(ai), fds()
+{
+    this->init();
 }
 
 stream_socket::~stream_socket()
