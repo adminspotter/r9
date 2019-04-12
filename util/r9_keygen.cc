ca5af447335788e8582f1ebc7ca0a68ffd0f973f
Trinity Quirk <tquirk@ymb.net>
2019-04-11 22:48:27 -0700

    Add command-line processing for r9_keygen
    We'll add the command line processing as one big chunk.  None of
    the rest of the program exists quite yet, but at least it'll
    understand all the options we want it to have.

--- util/r9_keygen.cc
+++ util/r9_keygen.cc
@@ -32,15 +32,87 @@
 
 #include <config.h>
 
+#if HAVE_STDLIB_H
+#include <stdlib.h>
+#endif /* HAVE_STDLIB_H */
+#if HAVE_GETOPT_H
+#include <getopt.h>
+#endif /* HAVE_GETOPT_H */
+
 #include <iostream>
+
+#define ARGPARSE_RETURN 2
+
+bool do_client_key = false, do_server_key = false;
+std::string key_path;
 
 void show_banner(void)
 {
     std::cout << "r9_keygen v" PACKAGE_VERSION << std::endl;
 }
 
+void process_command_line(int argc, char **argv)
+{
+    const char *optstr = "csf:";
+    int opt;
+#if HAVE_GETOPT_LONG
+    int longindex = 0;
+    struct option options[] = {
+        { "client",  no_argument,        NULL,  'c' },
+        { "server",  no_argument,        NULL,  's' },
+        { "file",    required_argument,  NULL,  'f' },
+        { 0,         0,                  NULL,  0   }
+    };
+#endif /* HAVE_GETOPT_LONG */
+
+    while ((opt =
+#if HAVE_GETOPT_LONG
+            getopt_long(argc, argv, optstr, options, &longindex)
+#else
+            getopt(argc, argv, optstr)
+#endif /* HAVE_GETOPT_LONG */
+               ) != -1)
+    {
+        switch (opt)
+        {
+          case 'c':
+            if (do_server_key == false)
+                do_client_key = true;
+            else
+            {
+                std::cerr << "Can't do both server and client keys."
+                          << std::endl;
+                exit(ARGPARSE_RETURN);
+            }
+            break;
+          case 's':
+            if (do_client_key == false)
+                do_server_key = true;
+            else
+            {
+                std::cerr << "Can't do both server and client keys."
+                          << std::endl;
+                exit(ARGPARSE_RETURN);
+            }
+            break;
+          case 'f':
+            key_path = optarg;
+            break;
+          case '?':
+#if !HAVE_GETOPT_LONG
+            std::cerr << "Unknown option " << (char)opt << std::endl;
+#endif /* !HAVE_GETOPT_LONG */
+            exit(ARGPARSE_RETURN);
+          default:
+            std::cerr << "Error in argument parsing" << std::endl;
+            exit(ARGPARSE_RETURN);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     show_banner();
+    process_command_line(argc, argv);
     return 0;
 }
