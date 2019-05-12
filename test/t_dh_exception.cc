48a67918fc86ad6aeac2a6faa2077a09ffd1be54
Trinity Quirk <tquirk@ymb.net>
2019-05-12 09:00:13 -0700

    Add test skeleton for errors in DH proto functions
    We've got a good amount of error bailout, and we don't test any of
    it.  Since we have to mock out all of the OpenSSL functions that we
    use, we'll do the testing in a file separate from the functional
    tests.

---
+++ test/t_dh_exception.cc
@@ -0,0 +1,22 @@
+#include <tap++.h>
+
+using namespace TAP;
+
+#include "../proto/dh.h"
+
+void test_dh_shared_secret(void)
+{
+}
+
+void test_digest_message(void)
+{
+}
+
+int main(int argc, char **argv)
+{
+    plan(0);
+
+    test_dh_shared_secret();
+    test_digest_message();
+    return exit_status();
+}
