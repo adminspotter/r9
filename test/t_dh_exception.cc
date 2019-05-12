f0aa022a942e169bd8cc3676db383eb9da462a9e
Trinity Quirk <tquirk@ymb.net>
2019-05-12 11:44:02 -0700

    Add test for proto dh key context failure
    When we create a shared secret, we must create a key context.  If
    it fails, that will drop us into our first bailout condition, which
    requires us to mock the OPENSSL_free function.  It's a macro around
    CRYPTO_free, so that's our actual mock.  The signature of
    CRYPTO_free changes from 1.0.x to 1.1.x, so we need to take the
    differences into account.
    
    We'll also add a counter to make sure we're freeing appropriately.
    In some other bailout conditions, we do call it once more, so this
    will help us with those tests.

--- test/t_dh_exception.cc
+++ test/t_dh_exception.cc
@@ -7,8 +7,12 @@ using namespace TAP;
 #include "../proto/dh.h"
 
 struct dh_message msg;
+/* EVP_PKEY_CTX is an opaque handle, so we need to define something fake. */
+struct evp_pkey_ctx_st { int a; };
+EVP_PKEY_CTX ctx;
 
-bool malloc_error = false;
+bool malloc_error = false, pkey_new_error = false;
+int free_count = 0;
 
 #if OPENSSL_VERSION_NUMBER >= 0x1010000fL
 void *CRYPTO_malloc(size_t a, const char *b, int c)
@@ -21,6 +25,23 @@ void *CRYPTO_malloc(int a, const char *b, int c)
     return &msg;
 }
 
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+void CRYPTO_free(void *a, const char *b, int c)
+#else
+void CRYPTO_free(void *a)
+#endif
+{
+    free_count += 1;
+    return;
+}
+
+EVP_PKEY_CTX *EVP_PKEY_CTX_new(EVP_PKEY *a, ENGINE *b)
+{
+    if (pkey_new_error)
+        return NULL;
+    return &ctx;
+}
+
 void test_dh_shared_secret(void)
 {
     std::string test = "dh_shared_secret: ", st;
@@ -30,6 +51,15 @@ void test_dh_shared_secret(void)
     is(dh_shared_secret(NULL, NULL) == NULL,
        true,
        test + st + "expected result");
+
+    st = "pkey new error: ";
+    malloc_error = false;
+    pkey_new_error = true;
+    free_count = 0;
+    is(dh_shared_secret(NULL, NULL) == NULL,
+       true,
+       test + st + "expected result");
+    is(free_count, 1, test + st + "expected free calls");
 }
 
 void test_digest_message(void)
@@ -38,7 +68,7 @@ void test_digest_message(void)
 
 int main(int argc, char **argv)
 {
-    plan(1);
+    plan(3);
 
     test_dh_shared_secret();
     test_digest_message();
