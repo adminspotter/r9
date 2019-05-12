34a22168595990755467091f1609c15af392cf92
Trinity Quirk <tquirk@ymb.net>
2019-05-12 11:50:08 -0700

    Add proto test for dh derivation failures
    We initialize the derivation context, set the peer key, and perform
    the derivation.  If one of those fails, it drops us into the second
    of our bailout conditions.  This requires us to mock
    EVP_PKEY_CTX_free.  We add counting in the new mock to be sure our
    mock is being called.

--- test/t_dh_exception.cc
+++ test/t_dh_exception.cc
@@ -12,7 +12,9 @@ struct evp_pkey_ctx_st { int a; };
 EVP_PKEY_CTX ctx;
 
 bool malloc_error = false, pkey_new_error = false;
-int free_count = 0;
+bool pkey_derive_init_error = false, pkey_derive_peer_error = false;
+bool pkey_derive_error = false;
+int free_count = 0, pkey_ctx_free_count = 0;
 
 #if OPENSSL_VERSION_NUMBER >= 0x1010000fL
 void *CRYPTO_malloc(size_t a, const char *b, int c)
@@ -42,6 +44,33 @@ EVP_PKEY_CTX *EVP_PKEY_CTX_new(EVP_PKEY *a, ENGINE *b)
     return &ctx;
 }
 
+void EVP_PKEY_CTX_free(EVP_PKEY_CTX *a)
+{
+    pkey_ctx_free_count += 1;
+    return;
+}
+
+int EVP_PKEY_derive_init(EVP_PKEY_CTX *a)
+{
+    if (pkey_derive_init_error)
+        return 0;
+    return 1;
+}
+
+int EVP_PKEY_derive_set_peer(EVP_PKEY_CTX *a, EVP_PKEY *b)
+{
+    if (pkey_derive_peer_error)
+        return 0;
+    return 1;
+}
+
+int EVP_PKEY_derive(EVP_PKEY_CTX *a, unsigned char *b, size_t *c)
+{
+    if (pkey_derive_error)
+        return 0;
+    return 1;
+}
+
 void test_dh_shared_secret(void)
 {
     std::string test = "dh_shared_secret: ", st;
@@ -60,6 +89,37 @@ void test_dh_shared_secret(void)
        true,
        test + st + "expected result");
     is(free_count, 1, test + st + "expected free calls");
+
+    st = "derive_init error: ";
+    pkey_new_error = false;
+    pkey_derive_init_error = true;
+    pkey_ctx_free_count = free_count = 0;
+    is(dh_shared_secret(NULL, NULL) == NULL,
+       true,
+       test + st + "expected result");
+    is(free_count, 1, test + st + "expected free calls");
+    is(pkey_ctx_free_count, 1, test + st + "expected ctx free calls");
+
+    st = "derive_set_peer error: ";
+    pkey_derive_init_error = false;
+    pkey_derive_peer_error = true;
+    pkey_ctx_free_count = free_count = 0;
+    is(dh_shared_secret(NULL, NULL) == NULL,
+       true,
+       test + st + "expected result");
+    is(free_count, 1, test + st + "expected free calls");
+    is(pkey_ctx_free_count, 1, test + st + "expected ctx free calls");
+
+    st = "derive error: ";
+    pkey_derive_peer_error = false;
+    pkey_derive_error = true;
+    pkey_ctx_free_count = free_count = 0;
+    is(dh_shared_secret(NULL, NULL) == NULL,
+       true,
+       test + st + "expected result");
+    is(free_count, 1, test + st + "expected free calls");
+    is(pkey_ctx_free_count, 1, test + st + "expected ctx free calls");
+
 }
 
 void test_digest_message(void)
@@ -68,7 +128,7 @@ void test_digest_message(void)
 
 int main(int argc, char **argv)
 {
-    plan(3);
+    plan(12);
 
     test_dh_shared_secret();
     test_digest_message();
