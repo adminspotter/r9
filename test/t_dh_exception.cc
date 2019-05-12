efb55971f62e6112a09d5c463c504e1656cc0ed6
Trinity Quirk <tquirk@ymb.net>
2019-05-12 11:15:28 -0700

    Add mocking and testing for failed malloc in dh exception test
    OPENSSL_malloc, which is what the dh_shared_secret function calls,
    is actually a macro around CRYPTO_malloc, which we need to mock.
    The test itself is pretty straightforward.
    
    OpenSSL changed the signature of CRYPTO_malloc from 1.0.x to 1.1.x
    by changing the size parameter from int to size_t, so we do need
    to take that change into account.

--- test/t_dh_exception.cc
+++ test/t_dh_exception.cc
@@ -2,10 +2,34 @@
 
 using namespace TAP;
 
+#include <openssl/opensslv.h>
+
 #include "../proto/dh.h"
+
+struct dh_message msg;
+
+bool malloc_error = false;
+
+#if OPENSSL_VERSION_NUMBER >= 0x1010000fL
+void *CRYPTO_malloc(size_t a, const char *b, int c)
+#else
+void *CRYPTO_malloc(int a, const char *b, int c)
+#endif
+{
+    if (malloc_error)
+        return NULL;
+    return &msg;
+}
 
 void test_dh_shared_secret(void)
 {
+    std::string test = "dh_shared_secret: ", st;
+
+    st = "malloc error: ";
+    malloc_error = true;
+    is(dh_shared_secret(NULL, NULL) == NULL,
+       true,
+       test + st + "expected result");
 }
 
 void test_digest_message(void)
@@ -14,7 +38,7 @@ void test_digest_message(void)
 
 int main(int argc, char **argv)
 {
-    plan(0);
+    plan(1);
 
     test_dh_shared_secret();
     test_digest_message();
