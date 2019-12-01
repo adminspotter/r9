ee520d387eabcce7fb21eeb3e9d16b377000ae03
Trinity Quirk <tquirk@ymb.net>
2019-12-01 11:27:19 -0800

    Add keyboard control module
    The setup adds key-down and key-up callbacks to whatever Cuddly widget
    is passed in.  It's largely the same code as what's currently in
    client/client.cc.

---
+++ client/control/keyboard.cc
@@ -0,0 +1,92 @@
+/* keyboard.cc
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 03 Jan 2021, 10:25:09 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2021  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file defines the keyboard interface to control the client.
+ *
+ * Things to do
+ *
+ */
+
+#include "keyboard.h"
+
+#include "../comm.h"
+#include "../client_core.h"
+
+void keyboard::keyboard_callback(ui::active *a, void *call, void *client)
+{
+    ui::key_call_data *call_data = (ui::key_call_data *)call;
+    Comm *comm = (Comm *)client;
+
+    if (call_data->key == ui::key::u_arrow
+        || call_data->key == ui::key::d_arrow)
+    {
+        float val = (call_data->state == ui::key::up ? 0.0
+                     : (call_data->key == ui::key::u_arrow ? 1.0 : -1.0));
+        glm::vec3 move(0.0, val, 0.0);
+        comm->send_action_request(3, move, 100);
+    }
+    else if (call_data->key == ui::key::l_arrow
+             || call_data->key == ui::key::r_arrow)
+    {
+        glm::vec3 rot(0.0, 0.0, 0.0);
+
+        if (call_data->state == ui::key::down)
+        {
+            float val = (call_data->key == ui::key::l_arrow ? 1.0 : -1.0);
+            rot = glm::vec3(0.0, 0.0, val);
+        }
+        comm->send_action_request(4, rot, 100);
+    }
+}
+
+keyboard::keyboard()
+{
+}
+
+keyboard::~keyboard()
+{
+}
+
+void keyboard::setup(ui::active *comp, Comm *comm)
+{
+    comp->add_callback(ui::callback::key_down,
+                       keyboard::keyboard_callback,
+                       comm);
+    comp->add_callback(ui::callback::key_up,
+                       keyboard::keyboard_callback,
+                       comm);
+}
+
+void keyboard::cleanup(ui::active *comp, Comm *comm)
+{
+    comp->remove_callback(ui::callback::key_down,
+                          keyboard::keyboard_callback,
+                          comm);
+    comp->remove_callback(ui::callback::key_up,
+                          keyboard::keyboard_callback,
+                          comm);
+}
+
+extern "C" control *create(void)
+{
+    return (control *)new keyboard();
+}
---
+++ client/control/keyboard.h
@@ -0,0 +1,47 @@
+/* keyboard.h                                               -*- C++ -*-
+ *   by Trinity Quirk <tquirk@ymb.net>
+ *   last updated 29 Nov 2020, 20:10:38 tquirk
+ *
+ * Revision IX game client
+ * Copyright (C) 2020  Trinity Annabelle Quirk
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ *
+ * This file declares a class to use the keyboard to control the client.
+ *
+ * Things to do
+ *
+ */
+
+#ifndef __INC_R9CLIENT_KEYBOARD_H__
+#define __INC_R9CLIENT_KEYBOARD_H__
+
+#include "control.h"
+
+class keyboard : public control
+{
+  private:
+    static void keyboard_callback(ui::active *, void *, void *);
+
+  public:
+    keyboard();
+    ~keyboard();
+
+    virtual void setup(ui::active *, Comm *) override;
+    virtual void cleanup(ui::active *, Comm *) override;
+};
+
+#endif /* __INC_R9CLIENT_KEYBOARD_H__ */
