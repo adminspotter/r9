4007895aa02908fbaae6376d2862aa1f0df9041d
Trinity Quirk <tquirk@ymb.net>
2026-02-20 22:32:32 -0800

    Add default geometry for game objects with none
    When we create a new game object, we take a geometry pointer as an
    argument.  That pointer can be NULL, and we never check or make any
    effort to set some reasonable default.
    
    The geometry object now has a very simple default.  If we don't get a
    geometry object as a game object constructor argument, we'll take that
    very simple default.
    
    The game object destructor already cleans up any geometry objects, so
    we don't have to make any further changes in the destructor.

--- server/classes/game_obj.cc
+++ server/classes/game_obj.cc
@@ -2,7 +2,7 @@
  *   by Trinity Quirk <tquirk@ymb.net>
  *
  * Revision IX game server
- * Copyright (C) 2000-2021  Trinity Annabelle Quirk
+ * Copyright (C) 2000-2026  Trinity Annabelle Quirk
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -55,6 +55,8 @@ GameObject::GameObject(Geometry *g, Control *c, uint64_t newid)
     : position(), movement(), look(0.0, 1.0, 0.0),
       orient(1.0, 0.0, 0.0, 0.0), rotation(1.0, 0.0, 0.0, 0.0), movement_lock()
 {
+    if (g == NULL)
+        g = new Geometry();
     this->default_master = this->master = c;
     this->default_geometry = this->geometry = g;
     this->active = true;
